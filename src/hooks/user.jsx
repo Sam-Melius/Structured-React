288e6f199ddadcea58257c5f960f4f14413ce0c6
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-18 10:45:10 -0700

    created user.jsx hook

---
+++ src/hooks/user.jsx
@@ -0,0 +1,75 @@
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
+import { signInUser, signUpUser, signOutUser } from '../services/users';
+import { createProfile, updateProfile } from '../services/profiles';
+
+export const useAuth = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useAuth must be used within a UserProvider');
+      }
+
+      const { user, setUser } = context;
+
+      const isLoggedIn = user?.email;
+    
+      const signUp = async (email, password) => {
+        try {
+          const user = await signUpUser(email, password);
+          setUser(user);
+        }
+        catch (err) {
+          throw err;
+        }
+      };
+
+      const signIn = async (email, password) => {
+        try {
+          const user = await signInUser(email, password);
+          setUser(user);
+        }
+        catch (err) {
+          throw err;
+        }
+      };
+
+      const signOut = async () => {
+        await signOutUser();
+        setUser({});
+      };
+    
+      return { user, isLoggedIn, signUp, signIn, signOut };
+    };
+
+    export const useUser = () => {
+        const context = useContext(UserContext);
+      
+        if (context === undefined) {
+          throw new Error('useUser must be used within a UserProvider');
+        }
+
+  const { user, profile, setProfile, isLoaded } = context;
+
+  const create = async (data) => {
+    try {
+      const profile = await createProfile(data);
+      setProfile(profile);
+    }
+    catch (err) {
+      throw err;
+    }
+  };
+
+  const update = async (data) => {
+    try {
+      const profile = await updateProfile(data);
+      setProfile(profile);
+    }
+    catch (err) {
+      throw err;
+    }
+  };
+
+  return { user, profile, isLoaded, create, update };
+
+}
