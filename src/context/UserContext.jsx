6589354dff4216d96403a42e0a41ac744b8b4338
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-18 10:34:39 -0700

    created UserContext.jsx file

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useEffect, useState } from 'react';
+import { getUser } from '../services/users';
+import { getProfile } from '../services/profiles';
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    const currentUser = getUser();
+    const [user, setUser] = useState(
+        currentUser ? { id: currentUser.id, email: currentUser.email } : {}
+    );
+    const [profile, setProfile] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadProfile = async () => {
+      setIsLoaded(false);
+
+      try {
+        if(!user) return setProfile();
+
+        const profile = await getProfile(user.id);
+        setProfile(profile);
+      }
+      catch(err) {
+        setProfile(null);
+      }
+      finally {
+        setIsLoaded(true);
+      }
+    };
+
+    loadProfile();  
+  }, [user]);
+
+  const value = { user, setUser, profile, setProfile, isLoaded };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
