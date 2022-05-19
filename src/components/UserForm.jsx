379307bf26ca0d2a2aba55c3ff74aef0831599d3
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-18 17:32:30 -0700

    created UserForm component

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { userForm } from '../../hooks/userForm';
+
+export default function UserForm() {
+  const { formState, handleChange } = userForm({
+        email: '',
+        password: '',
+      });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password } = formState;
+
+    try {
+      setLoading(true);
+      await onSubmit(email, password);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <section>
+        <label htmlFor="email">Email</label>
+        <input
+            id="email"
+            type="email"
+            name="email"
+            required
+            value={formState.email}
+            onChange={handleChange}
+          />
+        </section>
+        <section>
+        <label htmlFor="password">Password</label>
+        <input
+            id="password"
+            type="password"
+            name="password"
+            required
+            value={formState.password}
+            onChange={handleChange}
+          />
+        </section>
+        <button type="submit" disabled={loading}>
+          {loading ? 'Loading...' : label}
+        </button>
+        
+    </form>
+  )
+}
