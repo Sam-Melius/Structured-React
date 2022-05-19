50bc083b6971c80625b71a00ee8832e63913dfee
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-18 17:25:17 -0700

    created userForm hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import { UserProvider } from "./context/UserContext";
+
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+    <UserProvider>
+      <Router>
+        <Switch>
+          <Route path='/login'></Route>
+        </Switch>
+      </Router>
+    </UserProvider>
+    </>
+  );
 }
